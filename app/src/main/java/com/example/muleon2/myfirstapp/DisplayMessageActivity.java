9360fc77df44023d499a018e4e4532d4b736b8c5
Kaihua Tan <muleon2@gmail.com>
2015-08-17 17:25:40 -0700

    signup page

--- app/src/main/java/com/example/muleon2/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/muleon2/myfirstapp/DisplayMessageActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
-    @Override
+    //@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_display_message);
@@ -21,14 +21,14 @@ public class DisplayMessageActivity extends AppCompatActivity {
         setContentView(textView);
     }
 
-    @Override
+    //@Override
     /*public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_display_message, menu);
         return true;
     }*/
 
-    @Override
+    //@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
