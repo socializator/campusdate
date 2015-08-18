2bf40ee2aab4377cf45879b15f56c08928700dbf
Kaihua Tan <muleon2@gmail.com>
2015-08-18 11:38:00 -0700

    lalala

--- app/src/main/java/com/example/muleon2/myfirstapp/SigninActivity.java
+++ app/src/main/java/com/example/muleon2/myfirstapp/SigninActivity.java
@@ -1,8 +1,13 @@
 package com.example.muleon2.myfirstapp;
 
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 /**
  * Created by muleon2 on 8/17/2015.
@@ -15,6 +20,37 @@ public class SigninActivity extends AppCompatActivity {
         setContentView(R.layout.signup_page);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+    /*public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }*/
 
 
 }
