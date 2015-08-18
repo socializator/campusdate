e7cc2f046df44345c4d45a049c340c57520b5b65
Kaihua Tan <muleon2@gmail.com>
2015-08-17 22:14:26 -0700

    signup page layout finished

---
+++ app/src/main/java/com/example/muleon2/myfirstapp/SigninActivity.java
@@ -0,0 +1,20 @@
+package com.example.muleon2.myfirstapp;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by muleon2 on 8/17/2015.
+ */
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_page);
+    }
+
+
+
+}
