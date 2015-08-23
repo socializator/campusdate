732c6807acd39cdf78f53884d78b8bd5b52836a3
Kaihua Tan <muleon2@gmail.com>
2015-08-23 00:36:22 -0700

    profile activity query added not finished

--- ParseStarterProject/src/main/java/com/parse/starter/ProfilePageActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/ProfilePageActivity.java
@@ -7,30 +7,83 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+
 public class ProfilePageActivity extends ActionBarActivity implements AdapterView.OnItemSelectedListener {
 
     public Spinner genderSpinner;
+    protected EditText firstNameEditText;
 
+    protected Button saveButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_page);
 
+        // /Spinner
         genderSpinner = (Spinner) findViewById(R.id.gender_spinner);
-
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.gender_items,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         genderSpinner.setAdapter(adapter);
-
         genderSpinner.setOnItemSelectedListener(this);
 
+        //Connect objects with ids
+        firstNameEditText = (EditText)findViewById(R.id.firstNameProfile);
+        saveButton = (Button)findViewById(R.id.saveButtonProfile);
+
+        //Listen to save button click
+        saveButton.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+
+                //get Current user's objectId
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                //String currentUserObjectIdID = currentUser.getObjectId();
+                String currentUserObjectIdID = "r22mHwUeTu";
+
+                //update an object
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("Profile");
+                // Retrieve the object by id
+
+                query.whereEqualTo("Me", currentUserObjectIdID);
+
+
+                query.getFirstInBackground(new GetCallback<ParseObject>() {
+
+                    String firstName = firstNameEditText.getText().toString();
+
+                    public void done(ParseObject profile, ParseException e) {
+                        if (e == null) {
+                            // Now let's update it with some new data. In this case, only cheatMode and score
+                            // will get sent to the Parse Cloud. playerName hasn't changed
+                            //if (firstName != null) {
+                            profile.put("FirstName", firstName);
+                            profile.saveInBackground();
+                            System.out.println("Success");
+
+                        } else {
+                            System.out.println("fail");
+                        }
+                    }
+                });
+
+            }
+        });
     }
 
     @Override
@@ -64,7 +117,7 @@ public class ProfilePageActivity extends ActionBarActivity implements AdapterVie
         // Another interface callback
     }
 
-    /*public void onCheckboxClicked(View view){
+    public void onCheckboxClicked(View view){
         // Is the view now checked?
         boolean checked = ((CheckBox) view).isChecked();
 
@@ -84,7 +137,7 @@ public class ProfilePageActivity extends ActionBarActivity implements AdapterVie
                 break;
 
         }
-    }*/
+    }
 
 
 }
