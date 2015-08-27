f10a12df00a13cb317ca190bf7199c1f45433ae5
Christian Memije <christianmemije@yahoo.com>
2015-08-27 00:30:37 -0700

    ad

--- ParseStarterProject/src/main/java/com/parse/starter/Swipe.java
+++ ParseStarterProject/src/main/java/com/parse/starter/Swipe.java
@@ -1,5 +1,6 @@
 package com.parse.starter;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -21,7 +22,7 @@ import com.parse.ParseUser;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Swipe extends ActionBarActivity {
+public class Swipe extends Activity {
 
     ArrayList<String> final_list = new ArrayList<String>();
 
