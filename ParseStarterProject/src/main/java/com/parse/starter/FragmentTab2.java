a60c496f780451fe7f06e8808a0524a24237dc53
stevensun <stevenshsd@gmail.com>
2015-08-27 12:12:24 -0700

    ADD TAB

---
+++ ParseStarterProject/src/main/java/com/parse/starter/FragmentTab2.java
@@ -0,0 +1,21 @@
+package com.parse.starter;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by stevensun on 8/27/15.
+ */
+public class FragmentTab2 extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.tab, container, false);
+        TextView textview = (TextView) view.findViewById(R.id.tabtextview);
+        textview.setText("Shit2");
+        return view;
+    }
+}
---
+++ ParseStarterProject/src/main/java/com/parse/starter/MyTabListener.java
@@ -0,0 +1,25 @@
+package com.parse.starter;
+import android.app.ActionBar;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+
+public class MyTabListener implements ActionBar.TabListener {
+    Fragment fragment;
+
+    public MyTabListener(Fragment fragment) {
+        this.fragment = fragment;
+    }
+
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        ft.replace(R.id.fragment_container, fragment);
+    }
+
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        ft.remove(fragment);
+    }
+
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        // nothing done here
+    }
+}
