230960b7f766c6543773ed04b2bc27a0aa79f9f5
Kaihua Tan <muleon2@gmail.com>
2015-08-27 03:48:34 -0700

    idk

--- ParseStarterProject/src/main/java/com/parse/starter/ProfilePageActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/ProfilePageActivity.java
@@ -1,7 +1,14 @@
 package com.parse.starter;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,7 +20,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
-
+import com.parse.starter.R;
 import com.parse.GetCallback;
 import com.parse.GetDataCallback;
 import com.parse.ParseException;
@@ -23,11 +30,15 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.io.FileNotFoundException;
+import java.net.URI;
 
-public class ProfilePageActivity extends Activity implements AdapterView.OnItemSelectedListener {
+
+public class ProfilePageActivity extends ActionBarActivity implements AdapterView.OnItemSelectedListener {
 
 
-    protected ImageButton profilePhotoImageButton;
+    protected ParseImageView parseImageView;
+    protected String photoPath;
 
     protected EditText firstNameEditText;
     protected EditText lastNameEditText;
@@ -46,7 +57,8 @@ public class ProfilePageActivity extends Activity implements AdapterView.OnItemS
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_page);
 
-
+        //profile photoes
+        parseImageView = (ParseImageView) findViewById(R.id.profile_photo);
 
         // /Spinner
         genderSpinner = (Spinner) findViewById(R.id.genderSpinnerProfile);
@@ -74,22 +86,22 @@ public class ProfilePageActivity extends Activity implements AdapterView.OnItemS
         /************* Retrieve Data From Parse Database *************/
         ParseUser currentUser = ParseUser.getCurrentUser();
         //String currentUserObjectIdID = currentUser.getObjectId();
-        String currentUserObjectIdID = "115TKypy3w";
+        String currentUserObjectIdID = "r22mHwUeTu";
 
-        ParseObject obj = ParseObject.createWithoutData("_User", currentUserObjectIdID);
+        //ParseObject obj = ParseObject.createWithoutData("_User", currentUserObjectIdID);
 
 
 
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Profile");
-        query.whereEqualTo("Me", obj);
+        query.whereEqualTo("user_object_id", currentUserObjectIdID);
         query.getFirstInBackground(new GetCallback<ParseObject>() {
             public void done(ParseObject object, ParseException e) {
                 if (e == null) {
 
+                    //get profile picture
                     ParseFile image = object.getParseFile("profile_picture");
-                    final ParseImageView imageView = (ParseImageView) findViewById(R.id.profile_photo);
-                    imageView.setParseFile(image);
-                    imageView.loadInBackground(new GetDataCallback() {
+                    parseImageView.setParseFile(image);
+                    parseImageView.loadInBackground(new GetDataCallback() {
                         public void done(byte[] data, ParseException e) {
                         }
                     });
@@ -108,8 +120,8 @@ public class ProfilePageActivity extends Activity implements AdapterView.OnItemS
 
                     genderSpinner.setSelection(adapter.getPosition(object.getString("gender")));
 
-                    boolean maleChecked = object.getBoolean("interested_in_male");
-                    boolean femaleChecked = object.getBoolean("interested_in_female");
+                    boolean maleChecked = object.getBoolean("interested_in_males");
+                    boolean femaleChecked = object.getBoolean("interested_in_females");
 
                     if (maleChecked == true) {
                         maleInterestCheckBox.setChecked(true);
@@ -129,6 +141,17 @@ public class ProfilePageActivity extends Activity implements AdapterView.OnItemS
             }
         });
 
+        /********** Upload an image to Parse Database **********/
+
+
+        parseImageView.setOnClickListener(new View.OnClickListener(){
+            public  void onClick(View view){
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,0);
+            }
+        });
+
 
 
         /************* Save Data to Parse Database *************/
@@ -140,15 +163,15 @@ public class ProfilePageActivity extends Activity implements AdapterView.OnItemS
                 //get Current user's objectId
                 ParseUser currentUser = ParseUser.getCurrentUser();
                 //String currentUserObjectIdID = currentUser.getObjectId();
-                String currentUserObjectIdID = "115TKypy3w";
+                String currentUserObjectIdID = "r22mHwUeTu";
 
-                ParseObject obj = ParseObject.createWithoutData("_User", currentUserObjectIdID);
+                //ParseObject obj = ParseObject.createWithoutData("_User", currentUserObjectIdID);
 
                 //update an object
                 ParseQuery<ParseObject> query = ParseQuery.getQuery("Profile");
                 // Retrieve the object by id
 
-                query.whereEqualTo("Me", obj);
+                query.whereEqualTo("user_object_id", currentUserObjectIdID);
 
                 query.getFirstInBackground(new GetCallback<ParseObject>() {
 
@@ -164,11 +187,20 @@ public class ProfilePageActivity extends Activity implements AdapterView.OnItemS
                     Boolean maleInterest = maleInterestCheckBox.isChecked();
                     Boolean femaleInterest = femaleInterestCheckBox.isChecked();
 
+
+
                     public void done(ParseObject profile, ParseException e) {
                         if (e == null) {
                             // Now let's update it with some new data. In this case, only cheatMode and score
                             // will get sent to the Parse Cloud. playerName hasn't changed
-                            //if (firstName != null) {
+
+                            byte[] image = "Working at Parse is great!".getBytes();
+                            ParseFile file = new ParseFile(photoPath,image);
+
+                            file.saveInBackground();
+                            profile.put("profile_picture", file);
+
+
                             profile.put("first_name", firstName);
                             profile.put("last_name", lastName);
                             profile.put("age", age);
@@ -177,12 +209,12 @@ public class ProfilePageActivity extends Activity implements AdapterView.OnItemS
 
                             profile.put("gender", gender);
 
-                            profile.put("interested_in_male", maleInterest);
-                            profile.put("interested_in_female", femaleInterest);
+                            profile.put("interested_in_males", maleInterest);
+                            profile.put("interested_in_females", femaleInterest);
 
                             profile.saveInBackground();
 
-                            System.out.println("Success");
+                            System.out.println("\nSuccess");
 
                         } else {
                             System.out.println("Fail");
@@ -192,6 +224,47 @@ public class ProfilePageActivity extends Activity implements AdapterView.OnItemS
 
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Uri targetUri = data.getData();
+            //photoPath = getRealPathFromURI(targetUri);
+            Bitmap bitmap;
+            try{
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                parseImageView.setImageBitmap(bitmap);
+                photoPath = getRealPathFromURI(targetUri);
+
+
+                //ParseUser currentUser = ParseUser.getCurrentUser();
+                //String currentUserObjectIdID = currentUser.getObjectId();
+                //String currentUserObjectIdID = "r22mHwUeTu";
+                //ParseQuery<ParseObject> query = ParseQuery.getQuery("Profile");
+                //query.whereEqualTo("user_object_id", currentUserObjectIdID);
+                //query.getFirst().put("profile_picture", file);
+
+            }catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    protected String getRealPathFromURI(Uri contentURI) {
+        String result;
+        Cursor cursor = getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) { // Source is Dropbox or other similar local file path
+            result = contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            result = cursor.getString(idx);
+            cursor.close();
+        }
+        return result;
     }
 
     @Override
