df2d46b5ff37fab24de9af51154f0fde533de660
Kaihua Tan <muleon2@gmail.com>
2015-09-01 18:48:17 -0700

    profile picture border added

--- ParseStarterProject/src/main/java/com/parse/starter/ProfilePageActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/ProfilePageActivity.java
@@ -8,7 +8,13 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -117,9 +123,6 @@ public class ProfilePageActivity extends Activity implements AdapterView.OnItemS
         /************* Retrieve Data From Parse Database *************/
         final ParseUser currentUser = ParseUser.getCurrentUser();
         final String currentUserObjectIdID = currentUser.getObjectId();
-        //String currentUserObjectIdID = "r22mHwUeTu";
-
-        //ParseObject obj = ParseObject.createWithoutData("_User", currentUserObjectIdID);
 
 
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Profile");
@@ -377,6 +380,7 @@ public class ProfilePageActivity extends Activity implements AdapterView.OnItemS
 
                 // Set the Image in ImageView after decoding the String
                 bitmap = BitmapFactory.decodeFile(photoPath);
+                bitmap = getRoundedCornerBitmap(bitmap, 53);
                 parseImageView.setImageBitmap(bitmap);
 
             } else {
@@ -474,5 +478,27 @@ public class ProfilePageActivity extends Activity implements AdapterView.OnItemS
     public void go_to_swipe(View view) {
         Intent intent = new Intent(this, Swipe.class);
         startActivity(intent);
+    }
+
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap, int pixels) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap
+                .getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = pixels;
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
     }
 }
